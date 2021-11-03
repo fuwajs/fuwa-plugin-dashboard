0c1f28d59e5f59b3887e9d172630d2597f49f5d8
Jamal <jamallyons8@gmail.com>
2021-11-03 14:03:00 -0400

    Live share updates
    Co-authored-by: Artrix <Artrix9095@users.noreply.github.com>

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,15 @@
+// imports, etc
 import { Nav, NavItem } from '../components/Nav';
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'; // BrowserRouter or HashRouter
 import ReactDOM from 'react-dom';
+// css
 import '../styles/nav.scss';
+import '../styles/content.scss'
 import '../styles/global.scss';
+// pages
+import Home from './pages/Home';
+// backend
 const ws = new WebSocket('ws://localhost:9094');
 
 const Index = () => {
@@ -15,16 +22,26 @@ const Index = () => {
         <NavItem icon="Settings" text="Bot settings" />,
     ];
 
+    
     return (
         <>
             <Nav type="left">{...navItems}</Nav>
+            <main id="main" role="main">
+                <Switch>
+                    <Route path="/" component={Home} />
+                    {/* TODO 404 PAGE */}
+                    {/* <Route path="*" component={} />  */}
+                </Switch>
+            </main>
         </>
     );
 };
 
 ReactDOM.render(
     <React.StrictMode>
-        <Index />
+        <Router>
+            <Index />
+        </Router>
     </React.StrictMode>,
     document.getElementById('root')
 );
---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Is this my home?</h1>
+            <p>
+                Why is this working?
+            </p>
+        </div>
+    );
+}
